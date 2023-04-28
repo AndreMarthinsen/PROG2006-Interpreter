e1c7966ceeebb5febdbb7058cd8a751753c313b1
andrrma <anddrma@stud.ntnu.no>
2023-04-28 12:57:37 +0200

    Adds a stack module with a basic implementation of a stack data structure with pop, top and push as its main features.
    Currently, iterators are publicly available. It might be better to enforce the semantics of a stack data structure by preventing the modification or access to elements that is not on the stack top at a later stage.
    
    Relates to #1

--- bprog-rust/src/main.rs
+++ bprog-rust/src/main.rs
@@ -1,3 +1,15 @@
+mod stack;
+
 fn main() {
-    println!("Hello, world!");
+    let mut stack = stack::Stack::new();
+
+
+    (0..10).map(|x| x.to_string())
+        .for_each(|x| stack.push(x));
+    println!("Stack size is {}", stack.size());
+    stack.pop().unwrap();
+    println!("Stack size is {}", stack.size());
+    stack.display_all_contents();
+    stack.push(String::from("5"));
+    assert_eq!("5", *stack.top().unwrap())
 }
---
+++ bprog-rust/src/stack.rs
@@ -0,0 +1,190 @@
+//! Summary goes here
+
+
+use std::fmt::{Debug, Display};
+
+#[derive(PartialEq, Clone, Debug)]
+/// Stack implements a traditional stack data structure with the
+/// usual top, pop, and push methods along with iterator methods.
+pub enum Stack<T: Clone + Display + Debug> {
+    Empty,
+    Top(T, Box<Stack<T>>, usize)
+}
+
+
+
+impl<T:Clone + Display + Debug> Stack<T> {
+
+    /// Prints the contents of the stack top to bottom left to right
+    pub fn display_all_contents(&self) {
+        let mut output = String::from("top: ");
+        self.iter().for_each(|x| {
+            output.push_str(&x.to_string());
+            output.push(' ');
+        });
+        output.push_str(" :bottom");
+        println!("{}", output);
+    }
+
+    /// Constructs a new empty stack
+    ///
+    /// ```
+    /// let mut stack = stack::Stack::new();
+    /// assert_eq!(Stack::Empty, stack);
+    /// ```
+    ///
+    pub fn new() -> Self {
+        Stack::Empty
+    }
+
+    /// Pushes a new value T onto the stack.
+    ///
+    /// # Arguments
+    /// `val`- An object of type T to be pushed onto the stack.
+    ///
+    /// ```
+    /// let mut stack = stack::Stack::new();
+    /// stack.push(5);
+    /// assert_eq!(5, *stack.top());
+    /// ```
+    ///
+    pub fn push(&mut self, val: T) {
+        match self {
+            Stack::Empty=> *self = Stack::Top(val, Box::new(Stack::Empty), 1),
+            Stack::Top(_, _, size) => {
+                let new_size = *size + 1;
+                let old_stack = Box::new(std::mem::replace(self, Stack::Empty));
+                *self = Stack::Top(val, old_stack, new_size)
+            }
+        }
+    }
+
+    /// Pops the top value off of the stack and returns the value.
+    ///
+    /// # Returns
+    ///
+    /// Some(T) if the stack is non-empty,
+    /// None otherwise.
+    ///
+    /// ```
+    /// let mut stack = stack::Stack::new();
+    /// stack.push(5);
+    /// let top = stack.pop().unwrap();
+    ///
+    /// assert_eq!(top, 6);
+    /// assert_eq!(Stack::Empty, stack);
+    /// ```
+    ///
+    pub fn pop(&mut self) -> Option<T> {
+        match std::mem::replace(self, Stack::Empty) {
+            Stack::Empty => None,
+            Stack::Top(val, bottom, _) => {
+                *self = *bottom;
+                Some(val)
+            }
+        }
+    }
+
+    /// Returns a reference to the top element without popping it off the stack.
+    ///
+    /// # Returns
+    /// Some(T) if stack is non-empty,
+    /// None otherwise.
+    ///
+    pub fn top(&self) -> Option<&T> {
+        match self {
+            Stack::Top(val, _ ,_) => Some(val),
+            _ => None
+        }
+    }
+
+    /// Checks if the stack is empty
+    ///
+    /// # Returns
+    ///
+    /// true if empty, false otherwise;
+    ///
+    /// ```
+    /// let stack = Stack::new();
+    /// assert_eq!(true, stack.is_empty());
+    ///
+    /// ```
+    pub fn is_empty(&self) -> bool {
+        match self {
+            Stack::Empty => true,
+            _ => false
+        }
+    }
+
+    /// Retrieves the element count of the stack
+    ///
+    /// # Returns
+    ///
+    /// The amount of elements currently on the stack
+    ///
+    pub fn size(&self) -> usize {
+        match self {
+            Stack::Empty => 0,
+            Stack::Top(_, _, size) => *size
+        }
+    }
+
+    /// Returns an iterator over the stacks contents.
+    pub fn iter(&self) -> impl Iterator<Item=&T> {
+        self.into_iter()
+    }
+}
+
+
+
+/// Implements StackIter
+pub struct StackIter<'a, T:Clone + Display + Debug> {
+    stack: &'a Stack<T>,
+}
+
+/// Implements IntoIterator for Stack, allowing the creation of an
+/// iterator for the Stack object.
+impl<'a, T:Clone + Display + Debug> IntoIterator for &'a Stack<T> {
+    type Item = &'a T;
+    type IntoIter = StackIter<'a, T>;
+    fn into_iter(self) -> Self::IntoIter {
+        StackIter { stack: self }
+    }
+}
+
+/// Implements FromIterator for Stack, allowing the use of .collect() to create
+///  a new stack from an iterator.
+impl<T:Clone + Display + Debug> FromIterator<T> for Stack<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut stack = Stack::Empty;
+        for val in iter {
+            stack.push(val);
+        }
+        stack
+    }
+}
+
+/// Implements Default value for a stack. Using Stack::new() will use the
+/// default to initialize the Stack object as Stack::Empty.
+impl<T:Clone + Display + Debug> Default for Stack<T> {
+    fn default() -> Self {
+        Stack::Empty
+    }
+}
+
+/// Implements an Iterator for StackIter
+impl <'a, T:Clone + Display + Debug> Iterator for StackIter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.stack {
+            Stack::Top(val, bottom,_) => {
+                self.stack = bottom;
+                Some(val)
+            }
+            Stack::Empty => {
+                None
+            }
+        }
+    }
+
+}
