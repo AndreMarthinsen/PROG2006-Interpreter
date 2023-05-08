42fbfcaec6c2ba8fd637d123e5b21de0b58e4916
andrrma <anddrma@stud.ntnu.no>
2023-05-08 21:59:49 +0200

    Adds doc tests to utility.rs

--- bprog-rust/src/utility.rs
+++ bprog-rust/src/utility.rs
@@ -10,7 +10,22 @@ use std::io::{BufReader, Read};
 ///
 /// # Returns
 ///
-/// Vec<String> on a successful read, io::Error otherwise.
+/// Vec\<String\> on a successful read, io::Error otherwise.
+///
+/// # Examples
+///
+/// ```
+/// use bprog::utility::get_input;
+/// use tempfile::tempfile;
+/// use std::io::{Seek, Write, SeekFrom};
+///
+/// let input_str = "Hello, world!";
+///     let mut input_file = tempfile::tempfile().unwrap();
+///     input_file.write_all(input_str.as_bytes()).unwrap();
+///     input_file.seek(SeekFrom::Start(0)).unwrap();
+///     let result = get_input(Some(&mut input_file));
+///     assert_eq!(result.unwrap(), input_str.to_string());
+/// ```
 ///
 pub fn get_input(in_file: Option<&mut File>) -> Result<String, io::Error> {
     let mut program_text = String::new();
@@ -29,7 +44,20 @@ pub fn get_input(in_file: Option<&mut File>) -> Result<String, io::Error> {
     }
 }
 
-pub fn to_tokens(input: &mut String) -> Vec<String> {
+/// Splits a string into tokens delimited by whitespace.
+///
+/// # Examples
+///
+/// ```
+/// use bprog::utility::to_tokens;
+/// let tokens: Vec<String> = vec!["one", "two", "three"]
+///     .into_iter().map(String::from).collect();
+///
+/// assert_eq!(tokens, to_tokens("one two three"));
+///
+/// ```
+///
+pub fn to_tokens(input: &str) -> Vec<String> {
     input
         .split_whitespace()
         .map(|s| s.to_string())
