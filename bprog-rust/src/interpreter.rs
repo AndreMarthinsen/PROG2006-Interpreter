ded44299fe01dc6ce6e469303a876ccba43d929e
andrrma <anddrma@stud.ntnu.no>
2023-05-08 21:21:13 +0200

    Some heavy-handed refactoring to reduce nesting, but does not alter functionality.
    #4

--- bprog-rust/src/interpreter.rs
+++ bprog-rust/src/interpreter.rs
@@ -3,7 +3,9 @@ use crate::op::{Op, Modifiers};
 use crate::parsed::Parsed;
 use crate::stack::Stack;
 use crate::stack_error::StackError;
-use crate::types::{Params, Constraint, Type};
+use crate::types::{Params};
+
+
 
 pub struct Binding {
     pub function: bool,
@@ -11,129 +13,169 @@ pub struct Binding {
     pub value: Parsed
 }
 
+
+
+enum Args {
+    Unary(Parsed),
+    Binary(Parsed, Parsed),
+    //Temary(Type, Type, Type)
+}
+
+
+
 pub fn run(stack: &mut Stack<Parsed>, input: &mut VecDeque<Parsed>, bindings: &mut HashMap<String, Binding>) {
-    while !input.is_empty() {
-        if let Some(p) = input.pop_front() {
-            match p.clone() {
-                Parsed::Error(e) => {
-                    println!("runtime error: {}", e);
-                    stack.push(p);
-                    break;
-                }
-                Parsed::Symbol(s) => {
-                    if let Some (val) = bindings.get(&s) {
-                        if val.function {
-                            run(stack, &mut VecDeque::from(val.value.get_contents().unwrap()), bindings)
-                        } else {
-                            stack.push(val.value.clone())
-                        }
+    while let Some(p) = input.pop_front() {
+        match p.clone() {
+            Parsed::Error(e) => {
+                println!("runtime error: {}", e);
+                stack.push(p);
+                break;
+            }
+            Parsed::Symbol(s) => {
+                if let Some (val) = bindings.get(&s) {
+                    if val.function {
+                        run(stack, &mut VecDeque::from(val.value.get_contents().unwrap()), bindings)
                     } else {
-                        stack.push(p.clone())
+                        stack.push(val.value.clone())
                     }
-                },
-                Parsed::List(s) => {
-                    stack.push(Parsed::List(s.iter().map(|p|
-                        match p {
-                        Parsed::Symbol(s) => {
-                            if let Some (val) = bindings.get(s.as_str()) {
-                                val.value.clone()
-                            } else {
-                                p.clone()
-                            }
-                        },
-                        _ => p.clone()
-                    }).collect()));
+                } else {
+                    stack.push(p.clone())
                 }
-                Parsed::Function(op) => {
-                    exec_op(op, stack, input, bindings )
-                },
-                other => {
-                    stack.push(other)
-                }
+            },
+            Parsed::List(s) => {
+                stack.push(Parsed::List(s.iter()
+                    .map(|p| resolve_symbol(p.clone(), bindings))
+                    .collect()));
+            }
+            Parsed::Function(op) => {
+                exec_op(op, stack, input, bindings )
+            },
+            other => {
+                stack.push(other)
             }
         }
     }
 }
+
 
 
 
 fn exec_op(op: Op, stack: &mut Stack<Parsed>, input: &mut VecDeque<Parsed>, bindings: &mut HashMap<String, Binding>) {
-    let signature = op.get_signature();
+    let signature = op.clone().get_signature();
+    let mut arg  = Parsed::Error(StackError::PopEmpty);
+    let mut arg2 = Parsed::Error(StackError::PopEmpty);
+    let mods: Modifiers;
+    let ret;
+
+    if let Ok(m) = get_modifiers(signature.modifiers, input, bindings) {
+        mods = m
+    } else {
+        stack.push(Parsed::Error(StackError::Undefined));
+        return
+    }
+
+    if let Params::Unary(_) | Params::Binary(_, _) = &signature.stack_args {
+        arg = stack.pop().unwrap_or_else(|| Parsed::Error(StackError::PopEmpty));
+    }
+    if let Params::Binary(_, _) = &signature.stack_args {
+        arg2 = stack.pop().unwrap_or_else(||Parsed::Error(StackError::PopEmpty));
+    }
     match &signature.stack_args {
         Params::Nullary => {
-            match get_closures(signature.modifiers, input) {
-                Ok (closures) => {
-                    let ret = op.exec_nullary(closures, bindings);
-                    if signature.ret.is_satisfied_by(&ret.get_type()) &&
-                        signature.ret != Constraint::Void {
-                        stack.push(ret)
-                    }
-                },
-                Err(e) => {
-                    stack.push(Parsed::Error(e));
-                }
-            }
-        }
+            ret = op.exec_nullary(mods, bindings);
+        },
         Params::Unary(c) => {
-            match get_closures(signature.modifiers, input) {
-                Ok(closures) => {
-                    let arg = stack.pop().unwrap();
-                    if c.is_satisfied_by(&arg.get_type()) {
-                        let res = op.exec_unary(arg, closures);
-                        //TODO: Error handling
-                        if signature.ret.is_satisfied_by(&res.get_type()) {
-                            match res {
-                                Parsed::Quotation(q) => {
-                                    run(stack, &mut q.clone(), bindings)
-                                },
-                                Parsed::Void => {},
-                                _ => stack.push(res)
-                            }
-                        } else {
-                            println!("{} {} {}", res, res.get_type(), signature.ret);
-                        };
-                    } else {
-                        print_mismatch_arg(op, signature.stack_args, Args::Unary(arg))
-                    }
-                },
-                Err(e) => {
-                    stack.push(Parsed::Error(e));
-                }
+            if !c.is_satisfied_by(&arg.get_type()) {
+                print_mismatch_arg(&op, signature.stack_args, &Args::Unary(arg));
+                return;
+            }
+            ret = op.exec_unary(arg, mods, bindings);
+        },
+        Params::Binary(c1, c2) => {
+            // Checks that the constraints of the function signature is satisfied.
+            if !c1.is_satisfied_by(&arg2.get_type()) ||
+                !c2.is_satisfied_by(&arg.get_type()) {
+                print_mismatch_arg(&op, signature.stack_args, &Args::Binary(arg2.clone(), arg.clone()))
+            }
+            ret = op.exec_binary(&arg2, &arg, mods, bindings);
+        },
+        _ => panic!("temary arguments not implemented")
+    }
+    if signature.ret.is_satisfied_by(&ret.get_type()) {
+        match ret {
+            Parsed::Quotation(q) => {
+                run(stack, &mut q.clone(), bindings)
+            },
+            Parsed::Void => {},
+            _ => stack.push(ret)
+        }} else {
+        println!("{} {} {}", ret, ret.get_type(), signature.ret);
+    };
+}
+
+
+
+fn get_modifiers(expected: Params, input: &mut VecDeque<Parsed>, bindings: &mut HashMap<String, Binding>)
+    -> Result<Modifiers, StackError> {
+
+    let mut mod1 = Parsed::Error(StackError::Undefined);
+    let mut mod2 = Parsed::Error(StackError::Undefined);
+    let mods;
+    if let Params::Unary(_) | Params::Binary(_,_) = expected {
+        if let Some( m) = input.pop_front() {
+            mod1 = resolve_symbol(m, bindings);
+        } else {
+            return Err(StackError::PopEmpty)
+        }
+    }
+    if let Params::Binary(_, _) = expected {
+        if let Some( m) = input.pop_front() {
+            mod2 = resolve_symbol(m, bindings);
+        }else {
+            return Err(StackError::PopEmpty)
+        }
+    }
+    match expected {
+        Params::Nullary => mods = Ok(Modifiers::None),
+        Params::Unary(constraint) => {
+            if constraint.is_satisfied_by(&mod1.get_type()) {
+                mods = Ok(Modifiers::Unary(mod1))
+            } else {
+                mods = Err(StackError::Undefined)
             }
         },
         Params::Binary(c1, c2) => {
-            let rhs = stack.pop().unwrap();
-            let lhs = stack.pop().unwrap();
-            // Checks that the constraints of the function signature is satisfied.
-            if c1.is_satisfied_by(&lhs.get_type()) &&
-                c2.is_satisfied_by(&rhs.get_type()) {
-                if let Ok(closures) = get_closures(signature.modifiers, input) {
-                    let res = op.exec_binary(&lhs, &rhs, closures, bindings);
-                    if signature.ret.is_satisfied_by(&res.get_type()) {
-                        match res {
-                            Parsed::Quotation(q) => {
-                                run(stack, &mut q.clone(), bindings)
-                            },
-                            Parsed::Void => {},
-                            _ => stack.push(res)
-                        }
-                    } else {
-                        println!("{}", res);
-                    };
-                }
-
+            if c1.is_satisfied_by(&mod1.get_type()) &&
+                c2.is_satisfied_by(&mod2.get_type()) {
+                mods = Ok(Modifiers::Binary(mod1, mod2))
             } else {
-                print_mismatch_arg(op, signature.stack_args, Args::Binary(
-                    lhs,
-                    rhs)
-                )
+                mods = Err(StackError::Undefined)
             }
         },
-        _ => {}
+        _ => panic!("closure arguments defined for max 2 quotations")
+    }
+    mods
+}
+
+
+
+fn resolve_symbol(sym: Parsed, bindings: &mut HashMap<String, Binding>) -> Parsed {
+    match sym {
+        Parsed::Symbol(s) => {
+            if let Some (binding) = bindings.get(&s) {
+                binding.value.clone()
+            } else {
+                Parsed::Symbol(s.clone())
+            }
+        },
+        _ => sym
     }
 }
 
-fn print_mismatch_arg(op: Op, exp: Params, got: Args) {
+
+
+
+fn print_mismatch_arg(op: &Op, exp: Params, got: &Args) {
     match (exp, got) {
         (Params::Unary(expected), Args::Unary(actual)) => {
             println!("err: argument of type \x1b[33m{}\x1b[0m with value \x1b[33m{}\x1b[0m does not satisfy constraint in \
@@ -171,57 +213,6 @@ fn print_mismatch_arg(op: Op, exp: Params, got: Args) {
         _ => {}
     }
 }
-
-enum Args {
-    Unary(Parsed),
-    Binary(Parsed, Parsed),
-    //Temary(Type, Type, Type)
-}
-
-
-fn get_closures (expected: Params, input: &mut VecDeque<Parsed>) -> Result<Modifiers, StackError> {
-    match expected {
-        Params::Nullary => {
-            Ok(Modifiers::None)
-        },
-        Params::Unary(constraint) => {
-            if let Some(val) = input.pop_front() {
-                if constraint.is_satisfied_by(&val.get_type()) {
-                    Ok(Modifiers::Unary(val))
-                } else {
-                    //TODO: Stack error
-                    Err(StackError::Undefined)
-                }
-            } else {
-                //TODO: Stack error
-                Err(StackError::Undefined)
-            }
-        },
-        Params::Binary(c1, c2) => {
-            let arg1 = input.pop_front();
-            let arg2 = input.pop_front();
-            match (arg1, arg2) {
-                (Some(val), Some(val2)) => {
-                    if c1.is_satisfied_by(&val.get_type()) &&
-                        c2.is_satisfied_by(&val2.get_type()) {
-                        Ok(Modifiers::Binary(val, val2))
-                    } else {
-                        //TODO:: STack Error
-                        Err(StackError::Undefined)
-                    }
-                },
-                //TODO: Stack error
-                _ => Err(StackError::Undefined)
-            }
-        },
-        _ => panic!("closure arguments defined for max 2 quotations")
-
-    }
-}
-
-
-
-
 
 
 
