bbd8d18b0089823c1853930523f9768af32a7c65
andrrma <anddrma@stud.ntnu.no>
2023-05-08 21:18:41 +0200

    Removes redundant function and uses get_input and to_tokens in its place.
    #2

--- bprog-rust/src/utility.rs
+++ bprog-rust/src/utility.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io;
 use std::io::{BufReader, Read};
 
-/// get_tokens retrieves text tokens from a specified file or STDIN
+/// get_tokens retrieves text from a specified file or STDIN
 ///
 /// # Arguments
 ///
@@ -12,27 +12,6 @@ use std::io::{BufReader, Read};
 ///
 /// Vec<String> on a successful read, io::Error otherwise.
 ///
-pub fn get_tokens(in_file: Option<&mut File>) -> Result<Vec<String>, io::Error> {
-    let mut program_text = String::new();
-    let read = match in_file {
-        Some(f) => {
-            let mut reader = BufReader::new(f);
-            reader.read_to_string(&mut program_text)
-        },
-        None => io::stdin().read_line(&mut program_text)
-    };
-    return match read {
-        Ok(_) => {
-            let tokens = program_text
-                .split_whitespace()
-                .map(|s| s.to_string())
-                .collect::<Vec<_>>();
-            Ok(tokens)
-        }
-        Err(e) => Err(e)
-    }
-}
-
 pub fn get_input(in_file: Option<&mut File>) -> Result<String, io::Error> {
     let mut program_text = String::new();
     let read = match in_file {
